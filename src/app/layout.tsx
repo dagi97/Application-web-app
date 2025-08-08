bd52bcbbd42234c4e2b390d5ad7d5aad2dabbcbe
Yosef Solomon <yosefsolomon21@gmail.com>
2025-08-08 23:24:49 +0300

    small fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider>{children}</Provider>
-        <Provider>{children}</Provider>
       </body>
     </html>
   );
